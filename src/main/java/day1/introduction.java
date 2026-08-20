8f6a3f3a756604afd396b3e4c204c3547751372f
Daksh Sharma <dakshsharma1136@gmail.com>
2026-08-21 00:32:18 +0530

    Day 1: Java fundamentals

---
+++ src/main/java/day1/introduction.java
@@ -0,0 +1,13 @@
+package day1;
+public class introduction {
+    public static void main(String[]args){
+
+            System.out.println("hi this is daksh sharma ");
+        System.out.println("this is my first code file  \ni am preparing for backend internship and want to crack the backend internship in top MNC's ");
+        System.out.println("this will take approx two and half months for the whole preperation including  project and learning ");
+        System.out.println("the whole road map is on NOTION that make chart and give whoe detail of today's task which help me alot");
+        System.out.println("THANK YOU");
+        System.out.println("DAKSH SHARMA");
+    }
+
+}
---
+++ src/main/java/day1/variables.java
@@ -0,0 +1,8 @@
+package day1;
+
+public class variables {
+    public static void main() {
+
+
+    }
+}
