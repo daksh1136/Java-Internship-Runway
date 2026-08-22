5ba6e1d2c78525e91e0be8a6794311af2913a3d7
Daksh Sharma <dakshsharma1136@gmail.com>
2026-08-22 12:21:53 +0530

    Day2: Java conditios

--- src/main/java/day2/ATm.java
+++ src/main/java/day2/ATm.java
@@ -1,7 +1,43 @@
 package day2;
+import java.util.*;
 
 public class ATm {
     static void main() {
-        System.out.println("hii");
+        int value=1000;
+
+        Scanner Sc=new Scanner(System.in);
+
+        while(true) {
+            System.out.println("hii");
+            System.out.println("Welcome To Lena Dena Bank!!!!!!");
+            System.out.println(" 1.view \n 2.deposit\n  3.widhrawal");
+            System.out.println("what do u want to do from above option: ");
+            int note=Sc.nextInt();
+            switch (note) {
+                case 1:
+                    System.out.println("your currrent balance is " + value);
+                    break;
+
+
+                case 2:
+                    int depos = Sc.nextInt();
+
+                    System.out.println("enter your cassh amount");
+
+                    value = value + depos;
+                    System.out.println("deposited");
+                    break;
+
+                case 3:
+                    System.out.println("enter amount  widhrawl");
+                    int withdrawl = Sc.nextInt();
+                    value = value - withdrawl;
+                    System.out.println("witshrwal the amount ");
+
+                default:
+                    break;
+            }
+        }
+
     }
 }
--- src/main/java/day2/menu.java
+++ src/main/java/day2/menu.java
@@ -1,4 +1,42 @@
 package day2;
+import java.util.Scanner;
+import java.util.Scanner;
 
 public class menu {
+    static void main() {
+        Scanner sc=new Scanner(System.in);
+
+        while (true) {
+            System.out.println("welcome to calculator");
+            System.out.println("1. add\n 2. sub\n 3.mult\n  4. divide");
+            System.out.println("enter your choice:");
+            int choice= sc.nextInt();
+            System.out.println("enter the  first number");
+            int n1= sc.nextInt();
+            System.out.println("enter the second number");
+            int n2= sc.nextInt();
+
+
+            switch (choice) {
+                case 1:
+                    System.out.println(n1+n2);
+                    break;
+                    case 2:
+                        System.out.println(n1-n2);
+                        break;
+                case 3:
+                    System.out.println(n1*n2);
+                    break;
+                case 4:
+                    System.out.println(n1/n2);
+                    break;
+                default:
+                    break;
+
+
+
+            }
+        }
+
+    }
 }
